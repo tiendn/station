2f45a7eed635b10eeb1a17568c780237975fa9fc
0xTylerD26 <182126836+0xTylerD26@users.noreply.github.com>
2024-09-30 14:23:41 +0700

    fix: generate new wallet error

--- craco.config.js
+++ craco.config.js
@@ -17,7 +17,8 @@ module.exports = {
 			webpackConfig.resolve.fallback = {
 				"crypto": require.resolve('crypto-browserify'),
 				"vm": require.resolve("vm-browserify"),
-				"buffer": require.resolve("buffer")
+				"buffer": require.resolve("buffer"),
+				"stream": require.resolve("stream-browserify")
 				// Add other fallbacks if needed
 			};
 			
--- src/auth/modules/create/NewWalletForm.tsx
+++ src/auth/modules/create/NewWalletForm.tsx
@@ -4,11 +4,9 @@ import CreateWalletWizard from "./CreateWalletWizard"
 import Quiz from "./Quiz"
 
 const NewWalletForm = () => {
-  const { mnemonic } = useMemo(() => new MnemonicKey(), []) // must be memoized
+	const { mnemonic } = useMemo(() => new MnemonicKey(), []); // must be memoized
 
-  return (
-    <CreateWalletWizard defaultMnemonic={mnemonic} beforeCreate={<Quiz />} />
-  )
+	return <CreateWalletWizard defaultMnemonic={mnemonic} beforeCreate={<Quiz />} />;
 }
 
 export default NewWalletForm
