7042e877e2649fc013ac204539ecabee26e4b425
Tyler <0xtylerd26@gmail.com>
2024-11-24 23:42:47 +0700

    chore: add version commit show on UI

--- src/app/sections/Aside.tsx
+++ src/app/sections/Aside.tsx
@@ -5,11 +5,14 @@ import styles from "./Aside.module.scss"
 
 const Aside = () => {
   return (
-    <Grid gap={20} className={styles.aside}>
-      <Links />
-      <LastHeight />
-    </Grid>
-  )
+		<Grid gap={20} className={styles.aside}>
+			<Links />
+			<LastHeight />
+			<p style={{ fontSize: 11, color: "hsl(240 9% 66%)", opacity: 0.75 }}>
+				Commit {process.env.REACT_APP_VERSION}
+			</p>
+		</Grid>
+  );
 }
 
 export default Aside
