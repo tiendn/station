e7dfb5ddd1815e28ed9331c1c246b2520143a332
Tyler <0xtylerd26@gmail.com>
2025-02-09 23:33:58 +0700

    fix: error create new proposal in estimate gas

--- src/txs/gov/SubmitProposalForm.tsx
+++ src/txs/gov/SubmitProposalForm.tsx
@@ -479,7 +479,7 @@ export default SubmitProposalForm
 
 /* fee estimation */
 const ESTIMATE = {
-  TITLE: "Lorem ipsum",
-  DESCRIPTION:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-}
+	TITLE: "Lorem ipsum",
+	DESCRIPTION:
+		"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+};
